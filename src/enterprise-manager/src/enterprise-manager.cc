d470860e736671b24b6d0b5da3b11136974f2fa2
mllimo <ar.guijarro.contreras@gmail.com>
2022-02-14 14:14:20 +0000

    Generando setup del proyecto con CMAKE y CONAN

---
+++ src/enterprise-manager/src/enterprise-manager.cc
@@ -0,0 +1,6 @@
+#include <enterprise/test.h>
+
+int main() {
+  test();
+  return 0;
+}
---
+++ src/enterprise/src/test.cc
@@ -0,0 +1,5 @@
+#include <enterprise/test.h>
+
+void test() {
+  std::cout << "Hello, world!" << std::endl;
+}
